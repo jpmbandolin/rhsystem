60326d38cf69e672b0c2844fbaa409d6af857351
João Paulo Bandolin <bandolin.jpmb@gmail.com>
2022-07-30 12:07:24 -0300

    Adicionada integração com o Discord

---
+++ api/ApplicationBase/Infra/DiscordIntegration/WebhookNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ApplicationBase\Infra\DiscordIntegration;
+
+class WebhookNotification
+{
+	/**
+	 * @var Embed[]
+	 */
+	private array $embeds;
+	
+	public function __construct(private readonly string $webhookAddress, private readonly string $username, Embed ...$embeds)
+	{
+		$this->embeds = $embeds;
+	}
+	
+	/**
+	 * @return void
+	 * @throws \JsonException
+	 */
+	public function send():void
+	{
+		$curl = curl_init();
+		
+		curl_setopt_array(
+			$curl, [
+			     CURLOPT_URL => $this->webhookAddress,
+			     CURLOPT_RETURNTRANSFER => true,
+			     CURLOPT_ENCODING => '',
+			     CURLOPT_MAXREDIRS => 10,
+			     CURLOPT_TIMEOUT => 0,
+			     CURLOPT_FOLLOWLOCATION => true,
+			     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			     CURLOPT_CUSTOMREQUEST => 'POST',
+			     CURLOPT_POSTFIELDS => json_encode($this->getBuildedPayload(), JSON_THROW_ON_ERROR),
+			     CURLOPT_HTTPHEADER => [
+				     'Content-Type: application/json',
+			     ],
+		     ]
+		);
+		
+		$response = curl_exec($curl);
+		
+		curl_close($curl);
+		echo $response;
+		
+	}
+	
+	/**
+	 * @return array
+	 */
+	private function getBuildedPayload(): array
+	{
+		$payload = ["username" => $this->username, "embeds" => []];
+		
+		foreach ($this->embeds as $index => $embed) {
+			$payload["embeds"][$index] = [
+				"title" => $embed->getTitle(), "type" => $embed->getType(), "description" => $embed->getDescription(), "color" => $embed->getColor(),
+			];
+		}
+		
+		return $payload;
+	}
+}
--- api/ApplicationBase/Infra/Slim/SlimErrorHandler.php
+++ api/ApplicationBase/Infra/Slim/SlimErrorHandler.php
@@ -8,7 +8,9 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Handlers\ErrorHandler;
 use Throwable;
+use ApplicationBase\Infra\DiscordIntegration\Embed;
 use ApplicationBase\Infra\Exceptions\RuntimeException;
+use ApplicationBase\Infra\DiscordIntegration\WebhookNotification;
 
 class SlimErrorHandler extends ErrorHandler
 {
@@ -46,6 +48,12 @@ class SlimErrorHandler extends ErrorHandler
 		$response->getBody()->write(
 			json_encode($payload, JSON_THROW_ON_ERROR | JSON_UNESCAPED_UNICODE)
 		);
+		
+		try {
+			$this->notificateDiscord($exception);
+		}catch (Throwable){
+		
+		}
 
 		return $response
 			->withStatus($exception->getHttpStatusCode())
@@ -53,5 +61,25 @@ class SlimErrorHandler extends ErrorHandler
 			->withHeader('Access-Control-Allow-Origin', '*')
 			->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
 			->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+	}
+	
+	/**
+	 * @param Throwable $t
+	 *
+	 * @return void
+	 * @throws JsonException
+	 */
+	private function notificateDiscord(Throwable $t):void{
+		$embeds = [];
+		foreach (AppException::yieldExceptionDataRecursive($t) as $exceptionData){
+			$embeds[] = new Embed(
+				title: $exceptionData['message'],
+				type: "rich",
+				description: "File: " . $exceptionData['file'] . " Line: " . $exceptionData['line'],
+				color: "255"
+			);
+		}
+		global $ENV;
+		(new WebhookNotification($ENV["APPLICATION"]['error_webhook_address'], "Bad News Bringer", ...$embeds))->send();
 	}
 }
