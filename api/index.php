0c358df618e55c0a33aed84b6bfe3a217d5f1ab1
Bandolin <bandolin.jpmb@gmail.com>
2022-12-23 17:41:55 -0300

    Added named parameters to Environment constructor to avoid mistakes

--- api/index.php
+++ api/index.php
@@ -17,22 +17,33 @@ use ApplicationBase\Infra\WhiteList\RedisWhiteList;
 $ENV = parse_ini_file('../env.ini', true);
 
 Environment::setupEnvironment(
-    new Application(
-        $ENV['APPLICATION']['environment'],
-        $ENV['APPLICATION']['token_whitelist'],
-        $ENV['APPLICATION']['error_webhook_address']
+    application: new Application(
+       environment:  $ENV['APPLICATION']['environment'],
+        tokenWhitelist: $ENV['APPLICATION']['token_whitelist'],
+        errorWebhookAddress: $ENV['APPLICATION']['error_webhook_address']
     ),
-    new Database(
-        $ENV['DATABASE']['database'],
-        $ENV['DATABASE']['user'],
-        $ENV['DATABASE']['password'],
-        $ENV['DATABASE']['host']
+    database: new Database(
+        database: $ENV['DATABASE']['database'],
+        user: $ENV['DATABASE']['user'],
+        password: $ENV['DATABASE']['password'],
+        host: $ENV['DATABASE']['host']
     ),
-    new FileStorage($ENV['FILE_STORAGE']['base_path']),
-    new Jwt($ENV['JWT']['key'], $ENV['JWT']['expires_at']),
-    new Migrations($ENV['MIGRATIONS']['db_host']),
-    new \ApplicationBase\Infra\Environment\Redis($ENV["REDIS"]['host']),
-    new Slim($ENV["SLIM"]['base_path'])
+    fileStorage: new FileStorage(
+        basePath: $ENV['FILE_STORAGE']['base_path']
+    ),
+    jwt: new Jwt(
+        key: $ENV['JWT']['key'],
+        expiresAt: $ENV['JWT']['expires_at']
+    ),
+    migrations: new Migrations(
+        dbHost: $ENV['MIGRATIONS']['db_host']
+    ),
+    redis: new \ApplicationBase\Infra\Environment\Redis(
+        host: $ENV["REDIS"]['host']
+    ),
+    slim: new Slim(
+        basePath: $ENV["SLIM"]['base_path']
+    )
 );
 
 $environment = Environment::getEnvironment();
